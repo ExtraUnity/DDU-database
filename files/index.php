260297acbf936008185b3a725050b3ad6329ad66
Chrilleballe <cvedelpetersen@gmail.com>
2020-09-22 15:28:05 +0200

    #14 #7 #6
    der er lavet lidt html

---
+++ files/index.php
@@ -0,0 +1,13 @@
+<head>
+</head>
+<body>
+<h1 id="title">
+Welcome to HCØL Bilbioteket
+</h1>
+<button>
+	<a href="login.php">Login</a>
+</button>
+<button>
+	<a href="signup.php">Signup</a>
+</button>
+</body>
