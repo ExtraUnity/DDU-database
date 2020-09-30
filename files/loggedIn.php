5634e847cb9d7471caace35cacb374b178fa8a23
Emil <emilfranord@gmail.com>
2020-09-30 14:27:59 +0200

    #10 button to link

--- files/loggedIn.php
+++ files/loggedIn.php
@@ -25,6 +25,8 @@
 	 <?php 
 	 	if(isset($_POST['searchTitle']) && $_POST['searchTitle'] != NULL) {
 	 		$title = $_POST['searchTitle'];
+			$title = mysqli_real_escape_string($dbConnect, $title);
+
 
 	 		$sql = "SELECT * FROM book WHERE Title='$title' AND StudentId IS NULL";
 
@@ -37,7 +39,8 @@
 }
 	 	} else if(isset($_POST['searchAuthor']) && $_POST['searchAuthor'] != NULL) {
 
-	 		$author = $_POST['searchAuthor'];
+			$author = $_POST['searchAuthor'];
+			$author = mysqli_real_escape_string($dbConnect, $author);
 
 	 		$sql = "SELECT * FROM book WHERE Author='$author' AND StudentId IS NULL";
 
@@ -53,6 +56,10 @@
 	 <button>
 	 	<a href="logout.php">Logout</a> 
 	 </button>
+	 <button>
+		<a href="userBooks.php">Return Books</a>
+	</button>
+
 	 <?php 
 	 	
 	 		if($_SESSION['isAdmin'] == 1) {
