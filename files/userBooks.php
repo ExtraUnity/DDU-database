aa669dd70f025cf3e5f9ce497dcd0962c5eccf7e
Emil <emilfranord@gmail.com>
2020-09-30 14:09:36 +0200

    #10 return borrowed books

---
+++ files/userBooks.php
@@ -0,0 +1,39 @@
+<head>
+	<?php include "checkSession.php"; ?>
+	<?php include "dbConnect.php"; ?>
+</head>
+
+<body>
+        <?php
+                if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                        foreach ($_POST as $name => $value) {                          
+                                $sql = "UPDATE book SET StudentId=NULL WHERE BookId='$name'";
+                                $result = mysqli_query($dbConnect,$sql);
+                        }
+                }
+        ?>
+
+	<h1>
+	Welcome to your book list.
+	</h1>
+	<h2>
+	See and return books your books. 
+	</h2>
+
+	<?php
+	$student = $_SESSION['studentId'];
+	$sql = "SELECT Title, Author, BookId FROM book WHERE StudentId ='$student'";
+
+	$result = mysqli_query($dbConnect, $sql);
+	//echo $result;
+
+
+
+	while ($row = mysqli_fetch_array($result)) {
+    		echo $row['Title'] . " by " . $row['Author'];
+        	echo "<form action='' method='POST'><input type='submit' name='" . $row['BookId'] . "' value='Return Book'></form>";
+    	echo "<br>";
+}
+	?>	
+</body>
+
