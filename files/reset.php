d0dc0affb08cb55ca26a1269a06039d4041fc2a3
LukasMansa <60343072+LukasMansa@users.noreply.github.com>
2020-10-02 13:12:59 +0200

    #12
    Rettelser

--- files/reset.php
+++ files/reset.php
@@ -5,6 +5,7 @@
 	<?php
 		if(isset($_POST['yes'])) {
 			$sql = "DROP TABLE student";
+			$sql = "DROP TABLE book";
 			$result = mysqli_query($dbConnect, $sql);
 			header("Location: loggedIn.php");
 		} else if (isset($_POST['no'])) {
@@ -12,7 +13,7 @@
 		}
 	?>
 <h2>
-Are you sure you want to reset all students from HCØL library?
+Are you sure you want to reset all students and books from HCØL library?
 </h2>
 <form action="" method="POST">
 	<input type="submit" name="yes" value="Yes">
